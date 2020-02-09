454cbeb0666f0a218b302fcccbc12a553e2d5662
Jacob Christopher Wong <jcw41@sfu.ca>
2020-02-08 20:13:06 -0800

    changed file name

--- matlab scripts/angleToR.m
+++
@@ -1,4 +0,0 @@
-function R = angle_to_R(x, y, theta)
-    % Converts an angle to a 2x2 Rotation matrix
-    
-end
---
+++ matlab scripts/utoi.m
@@ -0,0 +1,8 @@
+function R = utoi(x, y, theta)
+    % "User form to internal form"
+    % uform (vec3): 3-tuple (x, y, theta)
+    % iform (frame): consists of (2x1) position vector and a (2x2) R
+    % you may represent the frame with a 3x3 homogeneous transform in which
+    % the third row is [0 0 1]
+    
+end
